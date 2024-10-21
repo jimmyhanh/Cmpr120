c8bad3e6d0e7f0cb9d36accdda85bb0f431a9b2b
jimmyhanh <124125489+jimmyhanh@users.noreply.github.com>
2024-10-20 18:34:19 -0700

    Add files via upload

---
+++ Chapter 2/Challenge1/main.cpp
@@ -0,0 +1,22 @@
+//Hung Anh Ho
+//2-21-2023
+//Description: Chapter 2 - SumOfTwoNumbers
+
+#include<iostream>;
+using namespace std;
+
+int main()
+{
+	//Input
+	int number1 = 50;
+	int number2 = 100;
+	int	total;
+
+	//Process
+	total = number1 + number2;
+
+	//Output
+	cout << "The sum of two numbers: " << total << "\n";
+
+	return 0;
+}
