3f3c24f5cb54a4959842b509a5b9360205575862
Hung Anh Ho <124125489+jimmyhanh@users.noreply.github.com>
2024-11-06 03:27:13 -0800

    Add files via upload

---
+++ Chapter 2/Challenge16/main.cpp
@@ -0,0 +1,20 @@
+//Hung Anh Ho
+//2-23-2023
+//Description: Chapter 2: Challenge 16 - Diamond Pattern
+
+#include<iostream>;
+using namespace std;
+
+int main()
+{
+	//Output
+	cout << "   *\n\n";
+	cout << "  ***\n\n";
+	cout << " *****\n\n";
+	cout << "*******\n\n";
+	cout << " *****\n\n";
+	cout << "  ***\n\n";
+	cout << "   *\n\n";
+
+	return 0;
+}
