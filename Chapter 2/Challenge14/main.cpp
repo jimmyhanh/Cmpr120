45256c006e2456767f280d7a4c0a2e6f4b318073
Hung Anh Ho <124125489+jimmyhanh@users.noreply.github.com>
2024-11-07 00:00:06 -0800

    Add files via upload

---
+++ Chapter 2/Challenge14/main.cpp
@@ -0,0 +1,26 @@
+//Hung Anh Ho
+//2-23-2023
+//Description: Chapter 2: Challenge 14 - Personal Information
+
+#include<string>;
+#include<iostream>;
+using namespace std;
+
+int main()
+{
+	//Input
+	string name = "Hung Anh Ho";
+	string address = "Apt 4, 12691 Morningside ave, Garden Grove, 92843.";
+	string phone = "657-431-9415";
+	string major = "Computer Science";
+
+	//Output
+	cout << "Personal information:\n";
+	cout << "Your name: " << name << "\n";
+	cout << "Your address: " << address << "\n";
+	cout << "Your phone number: " << phone << "\n";
+	cout << "Your college major: " << major << "\n";
+
+	return 0;
+
+}
